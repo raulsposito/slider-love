9e9a12860ba038c91a94e7ed7ada06375a8ac1c9
raulsposito <raulsposito@gmail.com>
2021-05-18 22:43:20 -0300

    add tailwind and header

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  // ...
+  plugins: [
+    // ...
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        // Configure your color palette here, see https://tailwindcss.com/docs/customizing-colors
+        indigo: {
+          DEFAULT: '#00B1A6',
+          50: '#98FFF9',
+          100: '#7EFFF7',
+          200: '#4BFFF4',
+          300: '#18FFF1',
+          400: '#00E4D6',
+          500: '#00B1A6',
+          600: '#007E76',
+          700: '#004B46',
+          800: '#001817',
+          900: '#000000',
+        },
+        primary: {
+          DEFAULT: '#00B1A7',
+        },
+      },
+    },
+  },
+}
